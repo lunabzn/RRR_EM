f0903846b83e252b4920e0b8cdda5b01bc48e07d
Luna <lunabouzon79@gmail.com>
2023-06-09 21:23:48 +0200

    ARCHIVOS PRAC

---
+++ EM-practica-2022-2023/Assets/Scripts/Movement/Components/FighterMovement.cs
@@ -0,0 +1,143 @@
+﻿using Unity.Netcode;
+using Unity.Netcode.Components;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Movement.Components
+{
+    [RequireComponent(typeof(Rigidbody2D)), 
+     RequireComponent(typeof(Animator)),
+     RequireComponent(typeof(NetworkObject))]
+    public sealed class FighterMovement : NetworkBehaviour, IMoveableReceiver, IJumperReceiver, IFighterReceiver
+    {
+        public float speed = 1.0f;
+        public float jumpAmount = 1.0f;
+
+        private Rigidbody2D _rigidbody2D;
+        private Animator _animator;
+        private NetworkAnimator _networkAnimator;
+        private Transform _feet;
+        private LayerMask _floor;
+
+        private Vector3 _direction = Vector3.zero;
+        private bool _grounded = true;
+        
+        private static readonly int AnimatorSpeed = Animator.StringToHash("speed");
+        private static readonly int AnimatorVSpeed = Animator.StringToHash("vspeed");
+        private static readonly int AnimatorGrounded = Animator.StringToHash("grounded");
+        private static readonly int AnimatorAttack1 = Animator.StringToHash("attack1");
+        private static readonly int AnimatorAttack2 = Animator.StringToHash("attack2");
+        private static readonly int AnimatorHit = Animator.StringToHash("hit");
+        private static readonly int AnimatorDie = Animator.StringToHash("die");
+
+        void Start()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _networkAnimator = GetComponent<NetworkAnimator>();
+
+            _feet = transform.Find("Feet");
+            _floor = LayerMask.GetMask("Floor");
+        }
+
+        void Update()
+        {
+            if (!IsOwner) return;
+            
+            _grounded = Physics2D.OverlapCircle(_feet.position, 0.1f, _floor);
+            _animator.SetFloat(AnimatorSpeed, this._direction.magnitude);
+            _animator.SetFloat(AnimatorVSpeed, this._rigidbody2D.velocity.y);
+            _animator.SetBool(AnimatorGrounded, this._grounded);
+        }
+
+        void FixedUpdate()
+        {
+            _rigidbody2D.velocity = new Vector2(_direction.x, _rigidbody2D.velocity.y);
+        }
+
+        public void Move(IMoveableReceiver.Direction direction)
+        {
+            MoveServerRpc(direction);
+        }
+
+        public void Jump(IJumperReceiver.JumpStage stage)
+        {
+            JumpServerRpc(stage);
+        }
+
+        public void Attack1()
+        {
+            Attack1ServerRpc();
+        }
+
+        public void Attack2()
+        {
+            Attack2ServerRpc();
+        }
+
+        public void TakeHit()
+        {
+            TakeHitServerRpc();
+        }
+
+        public void Die()
+        {
+            DieServerRpc();
+        }
+
+        [ServerRpc]
+        public void MoveServerRpc(IMoveableReceiver.Direction direction)
+        {
+            if (direction == IMoveableReceiver.Direction.None)
+            {
+                this._direction = Vector3.zero;
+                return;
+            }
+
+            bool lookingRight = direction == IMoveableReceiver.Direction.Right;
+            _direction = (lookingRight ? 1f : -1f) * speed * Vector3.right;
+            transform.localScale = new Vector3(lookingRight ? 1 : -1, 1, 1);
+        }
+
+        [ServerRpc]
+        public void JumpServerRpc(IJumperReceiver.JumpStage stage)
+        {
+            switch (stage)
+            {
+                case IJumperReceiver.JumpStage.Jumping:
+                    if (_grounded)
+                    {
+                        float jumpForce = Mathf.Sqrt(jumpAmount * -2.0f * (Physics2D.gravity.y * _rigidbody2D.gravityScale));
+                        _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                    }
+                    break;
+                case IJumperReceiver.JumpStage.Landing:
+                    break;
+            }
+        }
+
+        [ServerRpc]
+        public void Attack1ServerRpc()
+        {
+            _networkAnimator.SetTrigger(AnimatorAttack1);
+        }
+
+        [ServerRpc]
+        public void Attack2ServerRpc()
+        {
+            _networkAnimator.SetTrigger(AnimatorAttack2);
+        }
+
+        [ServerRpc]
+        public void TakeHitServerRpc()
+        {
+            _networkAnimator.SetTrigger(AnimatorHit);
+        }
+
+        [ServerRpc]
+        public void DieServerRpc()
+        {
+            _networkAnimator.SetTrigger(AnimatorDie);
+        }
+    }
+}
